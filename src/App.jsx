d8cd677214317cc4f958eba41fc918d5d0a301d5
Rajdeep2820 <rajdeepsinghpanwar1@gmail.com>
2025-04-19 15:14:32 +0530

    Intial Commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Button from '@mui/material/Button';
+import DeleteIcon from '@mui/icons-material/Delete';
+import Alert from '@mui/material/Alert';
+import SearchBox from './SearchBox';
+
+
+function App() {
+  return(
+  <>
+  <SearchBox/>
+  </>
+  )
+}
+
+export default App
---
+++ src/SearchBox.jsx
@@ -0,0 +1,52 @@
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import {useState} from "react"
+import "./SearchBox.css"
+export default function SearchBox(){
+    const API_URL = "https://api.openweathermap.org/data/2.5/weather"; 
+    const API_KEY = "cd3c59302d279de4530566dafea98986";
+    let [city , setCity] = useState("");
+
+    let getWeatherInfo = async (evt)=>{
+        let response = await fetch(`${API_URL}?q=${city}&appid=${API_KEY}&units=metric`);
+        let jsonResponse = await response.json();
+        console.log(jsonResponse);
+        let result = {
+            temp : jsonResponse.main.temp,
+            humidity : jsonResponse.main.humidity,
+            tempMin : jsonResponse.main.temp_min,
+            tempMax : jsonResponse.main.temp_max,
+            feelsLike : jsonResponse.main.feels_like,
+            weather : jsonResponse.weather[0].description
+        }
+        console.log(result);
+    }
+    let handleChange = (evt)=>{
+        setCity(evt.target.value);
+    }
+
+    let handleSubmit = (evt)=>{
+        evt.preventDefault();
+        console.log(city);
+        setCity("");
+        getWeatherInfo();
+    }
+    return(
+        <div>
+            <h2>Search For the Weather</h2>
+            <form onSubmit={handleSubmit}
+            className='Form'>
+            <TextField id="city" 
+            label="Search City Name" 
+            variant="outlined" 
+            required 
+            value={city} 
+            onChange={handleChange} />
+            &nbsp; &nbsp; &nbsp;
+
+            <Button variant="outlined"
+             type='submit'>Search</Button>
+            </form>
+        </div>
+    )
+}
